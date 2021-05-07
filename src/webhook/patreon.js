3113330eda9a20c9f49b330246c5684b6746ed0a
soruly <soruly@gmail.com>
2021-05-07 19:33:19 +0800

    Add user on webhook trigger

--- src/webhook/patreon.js
+++ src/webhook/patreon.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 import Knex from "knex";
 import crypto from "crypto";
 
@@ -8,6 +9,7 @@ const {
   SOLA_DB_PWD,
   SOLA_DB_NAME,
   WEBHOOK_PATREON_SECRET,
+  SENDGRID_API_KEY,
 } = process.env;
 
 const knex = Knex({
@@ -37,5 +39,59 @@ export default async (req, res) => {
     return;
   }
   await knex("webhook").insert({ type: "patreon", json: req.rawBody });
+
+  const {
+    data: {
+      attributes: { patron_status, email },
+    },
+    included,
+  } = req.body;
+  if (!email) {
+    continue;
+  }
+  if (patron_status === "active_patron") {
+    const rewardTierID = included
+      .filter((e) => e.type === "tier")
+      .sort((a, b) => b.attributes.amount - a.attributes.amount)
+      .map((e) => e.id)[0];
+    if (rewardTierID) {
+      const tier = (
+        await knex("tier").select("id").where("patreon_id", Number(rewardTierID)).limit(1)
+      )[0].id;
+      const rows = await knex("user").select("*").where("email", email).limit(1);
+      if (!rows.length) {
+        let hmac = crypto.createHmac("sha256", TRACE_API_SALT);
+        const plainPassword = crypto
+          .randomBytes(16)
+          .toString("base64")
+          .replace(/[^0-9a-zA-Z]/g, "");
+        const password = hmac.update(plainPassword).digest("hex");
+        hmac = crypto.createHmac("sha256", TRACE_API_SALT);
+        const api_key = hmac.update(crypto.randomBytes(16)).digest("hex");
+        await knex("user").insert({ email, password, api_key, tier });
+      } else {
+        await knex("user").where("email", email).update({ tier });
+      }
+    }
+  } else if (patron_status === "declined_patron") {
+    await knex("user").where("email", email).update({ tier: 0 });
+  }
+
+  // if (SENDGRID_API_KEY) {
+  //   await fetch("https://api.sendgrid.com/v3/mail/send", {
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: `Bearer ${SENDGRID_API_KEY}`,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       personalizations: [{ to: [{ email: "soruly@gmail.com" }] }],
+  //       from: { email: "no-reply@trace.moe" },
+  //       subject: "Sending with SendGrid is Fun",
+  //       content: [{ type: "text/plain", value: "and easy to do anywhere, even with cURL" }],
+  //     }),
+  //   });
+  // }
+
   res.json({});
 };
