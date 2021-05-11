1fe067028c864be34c8a624cb4507a6873357b92
soruly <soruly@gmail.com>
2021-05-11 10:23:07 +0800

    Fix prettier format

--- src/uploaded.js
+++ src/uploaded.js
@@ -1,13 +1,7 @@
 import Knex from "knex";
 
-const {
-  SOLA_DB_HOST,
-  SOLA_DB_PORT,
-  SOLA_DB_USER,
-  SOLA_DB_PWD,
-  SOLA_DB_NAME,
-  TRACE_ALGO,
-} = process.env;
+const { SOLA_DB_HOST, SOLA_DB_PORT, SOLA_DB_USER, SOLA_DB_PWD, SOLA_DB_NAME, TRACE_ALGO } =
+  process.env;
 
 const knex = Knex({
   client: "mysql",
