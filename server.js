af790004cb1033c605c0bdf4bc1bfe0fddda7f66
soruly <soruly@gmail.com>
2021-05-14 19:21:42 +0800

    Allow CORS with credentials

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   bodyParser.json({
