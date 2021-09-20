0ac2ab6aa004bc3dba4ca71c147710671921cd7b
soruly <soruly@gmail.com>
2021-09-20 18:17:49 +0800

    Add script for checking solr status and optimizing solr

---
+++ script/optimize.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import fetch from "node-fetch";
+import getSolrCoreList from "../lib/get-solr-core-list.js";
+
+for (const coreURLs of getSolrCoreList().reduce(
+  (acc, cur, index, array) => (index % 4 ? acc : [...acc, array.slice(index, index + 4)]),
+  []
+)) {
+  console.log(`Optimizing:\n${coreURLs.join("\n")}`);
+  await Promise.all(coreURLs.map((coreURL) => fetch(`${coreURL}/update?wt=json&optimize=true`)));
+}
---
+++ script/status.js
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import fetch from "node-fetch";
+
+const { SOLA_SOLR_LIST } = process.env;
+
+const status = (
+  await fetch(`${SOLA_SOLR_LIST}admin/cores?indexInfo=true&wt=json`).then((res) => res.json())
+).status;
+
+const cores = {
+  total: {
+    current: null,
+    hasDeletions: null,
+    segmentCount: 0,
+    numDocs: 0,
+    maxDoc: 0,
+    deletedDocs: 0,
+    size: 0,
+  },
+};
+for (let id in status) {
+  const {
+    name,
+    index: { current, hasDeletions, segmentCount, numDocs, maxDoc, deletedDocs, size },
+  } = status[id];
+  cores[name] = {
+    current,
+    hasDeletions,
+    segmentCount,
+    numDocs,
+    maxDoc,
+    deletedDocs,
+    size,
+  };
+  cores.total.segmentCount += segmentCount;
+  cores.total.numDocs += numDocs;
+  cores.total.maxDoc += maxDoc;
+  cores.total.deletedDocs += deletedDocs;
+  cores.total.size += Number(size.split(" ")[0]);
+}
+cores.total.size = `${cores.total.size.toFixed(2)} GB`;
+
+console.table(cores);
