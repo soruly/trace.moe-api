031a224ca709ebfe6484028dac9472e011a817a1
soruly <soruly@gmail.com>
2021-12-03 10:53:52 +0000

    Update search test case

--- src/search.test.js
+++ src/search.test.js
@@ -77,7 +77,7 @@ describe("without API Key", () => {
     expect(topResult.similarity).toBeGreaterThan(0.9);
   });
 
-  test("/search by file upload", async () => {
+  test("/search by Form Post", async () => {
     if (!fs.existsSync("32B15UXxymfSMwKGTObY5e.jpg")) {
       await fetch("https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg")
         .then((e) => e.arrayBuffer())
@@ -88,6 +88,37 @@ describe("without API Key", () => {
     const response = await request(app)
       .post("/search")
       .attach("image", "32B15UXxymfSMwKGTObY5e.jpg");
+    expect(response.statusCode).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/^application\/json/);
+    expect(typeof response.body.frameCount).toBe("number");
+    expect(typeof response.body.error).toBe("string");
+    expect(Array.isArray(response.body.result)).toBeTruthy();
+    const topResult = response.body.result[0];
+    expect(typeof topResult.anilist).toBe("number");
+    expect(typeof topResult.filename).toBe("string");
+    expect(typeof topResult.episode).toBe("number");
+    expect(typeof topResult.from).toBe("number");
+    expect(typeof topResult.to).toBe("number");
+    expect(typeof topResult.similarity).toBe("number");
+    expect(typeof topResult.video).toBe("string");
+    expect(typeof topResult.image).toBe("string");
+    expect(topResult.anilist).toBe(21034);
+    expect(topResult.episode).toBe(1);
+    expect(topResult.similarity).toBeGreaterThan(0.9);
+    await fs.remove("32B15UXxymfSMwKGTObY5e.jpg");
+  });
+
+  test("/search by file upload", async () => {
+    const response = await request(app)
+      .post("/search")
+      .set("Content-Type", "image/jpeg")
+      .send(
+        Buffer.from(
+          await fetch("https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg").then((e) =>
+            e.arrayBuffer()
+          )
+        )
+      );
     expect(response.statusCode).toBe(200);
     expect(response.headers["content-type"]).toMatch(/^application\/json/);
     expect(typeof response.body.frameCount).toBe("number");
