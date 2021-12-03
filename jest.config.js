75d57aff7673796953e639e1705c2f4fef58ba71
soruly <soruly@gmail.com>
2021-12-03 10:42:30 +0000

    Ignore webhook and worker test case

--- jest.config.js
+++ jest.config.js
@@ -26,9 +26,7 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/script/", "/src/webhook/", "/src/worker/"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
