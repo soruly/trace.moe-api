9f2bed10cdd9a8f95d874d2db6945751b17e029e
soruly <soruly@gmail.com>
2022-07-16 18:50:18 +0000

    Fix format

--- src/get-status.js
+++ src/get-status.js
@@ -3,20 +3,21 @@ import fetch from "node-fetch";
 const { SOLA_SOLR_LIST } = process.env;
 
 export default async (req, res) => {
-  try{  const statusList = (
-    await Promise.all(
-      SOLA_SOLR_LIST.split(",").map((solrUrl) =>
-        fetch(`${solrUrl}admin/cores?wt=json`)
-          .then((res) => res.json())
-          .then(({ status }) => ({ solrUrl, cores: Object.values(status) }))
+  try {
+    const statusList = (
+      await Promise.all(
+        SOLA_SOLR_LIST.split(",").map((solrUrl) =>
+          fetch(`${solrUrl}admin/cores?wt=json`)
+            .then((res) => res.json())
+            .then(({ status }) => ({ solrUrl, cores: Object.values(status) }))
+        )
       )
-    )
-  ).reduce((acc, cur) => {
-    acc[cur.solrUrl] = cur.cores;
-    return acc;
-  }, {});
-  return res.json(statusList);
-  }catch(e){
+    ).reduce((acc, cur) => {
+      acc[cur.solrUrl] = cur.cores;
+      return acc;
+    }, {});
+    return res.json(statusList);
+  } catch (e) {
     console.log(e);
     return res.status(503);
   }
