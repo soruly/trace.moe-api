12cbe88e5b7c9b7942b25f603baf3eaae19e7c52
soruly <soruly@gmail.com>
2022-07-16 18:34:49 +0000

    Improve error handling

--- src/get-status.js
+++ src/get-status.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const { SOLA_SOLR_LIST } = process.env;
 
 export default async (req, res) => {
-  const statusList = (
+  try{  const statusList = (
     await Promise.all(
       SOLA_SOLR_LIST.split(",").map((solrUrl) =>
         fetch(`${solrUrl}admin/cores?wt=json`)
@@ -15,5 +15,9 @@ export default async (req, res) => {
     acc[cur.solrUrl] = cur.cores;
     return acc;
   }, {});
-  res.json(statusList);
+  return res.json(statusList);
+  }catch(e){
+    console.log(e);
+    return res.status(503);
+  }
 };
