ec35f1f4ee243d58175a710a14b169fcb8c11502
soruly <soruly@gmail.com>
2022-08-04 10:20:09 +0000

    Add endpoint to check db status

--- src/get-status.js
+++ src/get-status.js
@@ -1,8 +1,22 @@
 import fetch from "node-fetch";
 
-const { SOLA_SOLR_LIST } = process.env;
+const { SOLA_SOLR_LIST, TRACE_ALGO } = process.env;
 
 export default async (req, res) => {
+  const knex = req.app.locals.knex;
+  const { id } = req.query;
+  if (id) {
+    if (!id.match(/\d+/)) {
+      return res.status(400).json({
+        error: "Invalid param id: must be a number",
+      });
+    }
+    const rows = await knex(TRACE_ALGO)
+      .where("path", "like", `${id}/%`)
+      .select("path", "status", "created");
+    return res.json(rows);
+  }
+
   try {
     const statusList = (
       await Promise.all(
@@ -10,6 +24,7 @@ export default async (req, res) => {
           fetch(`${solrUrl}admin/cores?wt=json`)
             .then((res) => res.json())
             .then(({ status }) => ({ solrUrl, cores: Object.values(status) }))
+            .catch((e) => res.status(503))
         )
       )
     ).reduce((acc, cur) => {
--- src/get-status.test.js
+++ src/get-status.test.js
@@ -1,6 +1,27 @@
 import "dotenv/config";
 import { default as request } from "supertest";
+import Knex from "knex";
 import app from "./app.js";
+
+const { SOLA_DB_HOST, SOLA_DB_PORT, SOLA_DB_USER, SOLA_DB_PWD, SOLA_DB_NAME } = process.env;
+
+beforeAll(async () => {
+  app.locals.knex = Knex({
+    client: "mysql",
+    connection: {
+      host: SOLA_DB_HOST,
+      port: SOLA_DB_PORT,
+      user: SOLA_DB_USER,
+      password: SOLA_DB_PWD,
+      database: SOLA_DB_NAME,
+      multipleStatements: true,
+    },
+  });
+});
+
+afterAll(async () => {
+  await app.locals.knex.destroy();
+});
 
 test("GET /status", async () => {
   const response = await request(app).get("/status");
@@ -23,4 +44,18 @@ test("GET /status", async () => {
   expect(numDocs).toBeGreaterThanOrEqual(0);
   expect(numDocs).toBeGreaterThanOrEqual(0);
   expect(lastModified).toBeInstanceOf(Date);
+});
+
+test("GET /status?id=a", async () => {
+  const response = await request(app).get("/status?id=a");
+  expect(response.statusCode).toBe(400);
+  expect(response.headers["content-type"]).toMatch(/^application\/json/);
+  expect(typeof response.body).toBe("object");
+});
+
+test("GET /status?id=1", async () => {
+  const response = await request(app).get("/status?id=1");
+  expect(response.statusCode).toBe(200);
+  expect(response.headers["content-type"]).toMatch(/^application\/json/);
+  expect(Array.isArray(response.body)).toBeTruthy();
 });
