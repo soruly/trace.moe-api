1a12d5ddf36d22d292299daabf4357c8407ec587
soruly <soruly@gmail.com>
2024-03-05 16:06:54 +0000

    Add test cases

---
+++ src/scan.test.js
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import { default as request } from "supertest";
+import Knex from "knex";
+import app from "./app.js";
+
+const { TRACE_ALGO, SOLA_DB_HOST, SOLA_DB_PORT, SOLA_DB_USER, SOLA_DB_PWD, SOLA_DB_NAME } =
+  process.env;
+
+beforeAll(async () => {
+  app.locals.knex = Knex({
+    client: "mysql",
+    connection: {
+      host: SOLA_DB_HOST,
+      port: SOLA_DB_PORT,
+      user: SOLA_DB_USER,
+      password: SOLA_DB_PWD,
+      database: SOLA_DB_NAME,
+      multipleStatements: true,
+    },
+  });
+  await app.locals.knex(TRACE_ALGO).truncate();
+});
+
+afterAll(async () => {
+  await app.locals.knex(TRACE_ALGO).truncate();
+  await app.locals.knex.destroy();
+});
+
+test("GET /scan", async () => {
+  const response = await request(app).get("/scan");
+  expect(response.statusCode).toBe(204);
+});
