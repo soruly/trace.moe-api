40e6d97adf150956e756c700daa6a5950abf193f
soruly <soruly@gmail.com>
2024-09-01 00:29:48 +0900

    Improve scan file method

--- src/scan.js
+++ src/scan.js
@@ -7,21 +7,27 @@ const { VIDEO_PATH } = process.env;
 export default async (req, res) => {
   const knex = req.app.locals.knex;
 
+  const dbFileList = (await knex("file").select("path")).map((e) => e.path);
   const videoFileList = (await fs.readdir(VIDEO_PATH, { recursive: true, withFileTypes: true }))
     .filter((file) => file.isFile() && [".mkv", ".mp4"].includes(path.extname(file.name)))
-    .map((e) => path.join(e.path, e.name));
-  if (videoFileList.length) {
-    await knex("file")
-      .insert(
-        videoFileList.map((e) => ({
-          path: e.replace(VIDEO_PATH, ""),
-          status: "UPLOADED",
-        })),
-      )
-      .onConflict("path")
-      .ignore();
+    .map((e) => path.join(e.path, e.name).replace(VIDEO_PATH, ""));
+  const newFileList = videoFileList.filter((e) => !dbFileList.includes(e));
+
+  if (newFileList.length) {
+    await knex("file").insert(
+      newFileList.map((e) => ({
+        path: e,
+        status: "UPLOADED",
+      })),
+    );
   }
-  res.sendStatus(204);
+
+  res.json({
+    fs: videoFileList.length,
+    db: dbFileList.length,
+    added: newFileList.length,
+    missing: dbFileList.filter((e) => !videoFileList.includes(e)).length,
+  });
 
   await startWorker(req.app);
 };
