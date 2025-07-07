b415df40e69ca606ef0352a7b889314bcbebf1c7
soruly <soruly@gmail.com>
2025-07-07 10:29:40 +0000

    Remove old scene detection with canvas

--- src/lib/detect-scene.js
+++ src/lib/detect-scene.js
@@ -1,8 +1,3 @@
-import os from "node:os";
-import path from "node:path";
-import child_process from "node:child_process";
-import fs from "node:fs/promises";
-import Canvas from "canvas";
 import getVideoDuration from "./get-video-duration.js";
 
 const { VIDEO_PATH } = process.env;
@@ -70,116 +65,10 @@ export default async (filePath, t, minDuration, maxDuration, knex) => {
     }
   }
 
-  const tBefore = 5;
-  const tAfter = 5;
-  let trimStart = t - tBefore;
-  let trimEnd = t + tAfter;
-  if (t - tBefore < 0) {
-    trimStart = 0;
-    trimEnd = tBefore + tAfter;
-  }
-  if (t + tAfter > videoDuration) {
-    trimStart = videoDuration - tBefore - tAfter;
-    trimEnd = videoDuration;
-  }
-  const fps = 12;
-  const width = 32;
-  const height = 18;
-
-  const tempPath = path.join(os.tmpdir(), `trace.moe-preview-${process.hrtime().join("")}`);
-  await fs.rm(tempPath, { recursive: true, force: true });
-  await fs.mkdir(tempPath, { recursive: true });
-  await new Promise((resolve) => {
-    const ffmpeg = child_process.spawn(
-      "ffmpeg",
-      [
-        "-y",
-        "-ss",
-        trimStart - 10,
-        "-i",
-        filePath,
-        "-ss",
-        "10",
-        "-t",
-        trimEnd - trimStart,
-        "-an",
-        "-vf",
-        `fps=${fps},scale=${width}:${height}`,
-        `${tempPath}/%04d.jpg`,
-      ],
-      { timeout: 10000 },
-    );
-    ffmpeg.on("close", resolve);
-  });
-
-  const imageDataList = await Promise.all(
-    (await fs.readdir(tempPath)).map(
-      (file) =>
-        new Promise(async (resolve) => {
-          const canvas = Canvas.createCanvas(width, height);
-          const ctx = canvas.getContext("2d");
-          const image = await Canvas.loadImage(path.join(tempPath, file));
-          ctx.drawImage(image, 0, 0, width, height);
-          resolve(ctx.getImageData(0, 0, width, height).data);
-        }),
-    ),
-  );
-  await fs.rm(tempPath, { recursive: true, force: true });
-
-  const getImageDiff = (a, b) => {
-    let diff = 0;
-    for (let i = 0; i < a.length; i++) {
-      diff += Math.abs(a[i] - b[i]);
-    }
-    return Math.floor(diff / 1000);
-  };
-
-  const frameInfo = imageDataList
-    .map((curr, index, array) => getImageDiff(curr, index ? array[index - 1] : curr))
-    .map((curr, index) => ({
-      id: index,
-      diff: curr,
-    }));
-
-  const threshold = 50;
-  let centerFrameID = Math.floor((t - trimStart) * fps);
-  if (centerFrameID > frameInfo.length - 1) {
-    centerFrameID = frameInfo.length - 1;
-  }
-  const minFrames = (minDuration / 2) * fps;
-
-  let startFrameID = centerFrameID;
-  let endFrameID = centerFrameID;
-  for (let i = centerFrameID; i >= 0; i--) {
-    // compare with prev frame
-    if (i === 0 || (frameInfo[i].diff > threshold && centerFrameID - i > minFrames)) {
-      startFrameID = i;
-      break;
-    }
-  }
-
-  for (let i = centerFrameID; i < frameInfo.length; i++) {
-    // compare with next frame
-    if (
-      i + 1 === frameInfo.length ||
-      (frameInfo[i + 1].diff > threshold && i - centerFrameID > minFrames)
-    ) {
-      endFrameID = i;
-      break;
-    }
-  }
-
-  // debug use
-  // frameInfo[centerFrameID] = Object.assign(frameInfo[centerFrameID], {center:true});
-  // frameInfo[startFrameID] = Object.assign(frameInfo[startFrameID], {start:true});
-  // frameInfo[endFrameID] = Object.assign(frameInfo[endFrameID], {end:true});
-  // console.log(frameInfo);
-  const sceneTrimStart = trimStart + startFrameID / fps;
-  const sceneTrimEnd = trimStart + endFrameID / fps;
-
+  // fallback to fixed scene cutting
   return {
-    start: sceneTrimStart,
-    end: sceneTrimEnd,
+    start: t - minDuration < 0 ? 0 : t - minDuration,
+    end: t + minDuration > videoDuration ? videoDuration : t + minDuration,
     duration: videoDuration,
   };
 };
