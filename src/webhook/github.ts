d5360e27b5ccad94298b47d6110070e7095f1d5b
soruly <soruly@gmail.com>
2026-03-01 02:33:59 +0000

    Improve error handling in github webhook

--- src/webhook/github.ts
+++ src/webhook/github.ts
@@ -5,6 +5,12 @@ import sql from "../../sql.ts";
 const { WEBHOOK_GITHUB_SECRET } = process.env;
 
 export default async (req, res) => {
+  if (!WEBHOOK_GITHUB_SECRET) {
+    console.error("[GitHub Webhook] WEBHOOK_GITHUB_SECRET is not set");
+    res.status(500).send("500 Internal Server Error");
+    return;
+  }
+
   const signature = req.header("X-Hub-Signature-256");
   if (!signature || !req.rawBody) {
     res.status(403).send("403 Forbidden");
@@ -19,11 +25,16 @@ export default async (req, res) => {
     res.status(403).send("403 Forbidden");
     return;
   }
-  await sql`
-    INSERT INTO
-      webhook (source, json)
-    VALUES
-      ('GITHUB', ${JSON.parse(req.rawBody)})
-  `;
-  res.json({});
+  try {
+    await sql`
+      INSERT INTO
+        webhook (source, json)
+      VALUES
+        ('GITHUB', ${JSON.parse(req.rawBody)})
+    `;
+    res.json({});
+  } catch (err) {
+    console.error("[GitHub Webhook] Unhandled error", err);
+    res.status(500).send("500 Internal Server Error");
+  }
 };
